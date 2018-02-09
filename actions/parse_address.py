90c0f6f3de659c370cbd5ea46ddf40c5787e4da4
Kenta Hattori <hattori@fixpoint.co.jp>
2018-02-09 14:23:30 +0900

    remove unused action

--- actions/parse_address.py
+++
@@ -1,9 +0,0 @@
-import sys
-import email.utils
-
-from st2common.runners.base_action import Action
-
-class ParseAddressAction(Action):
-    def run(self, address):
-        _, ret = email.utils.parseaddr(address)
-        return ret
